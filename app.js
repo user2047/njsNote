1e3614f1e5b5adca4e7ec5113ab3ade26165d226
user2047 <cameron.senna@gmail.com>
2018-03-10 19:58:10 -0500

    Second Day

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Server starting...")
+console.log("Server starting...");
+const fs = require("fs");
+const os = require("os");
+
+var user = os.userInfo();
+console.log("Hello " + user.username);
+
+fs.appendFile("greetings.txt", "Hello" + user.username + "!", (err) => {
+  if (err) {
+    console.log("unknown error");
+  }});
