81a08c459423b3bc1d9c2490b5370cd9cf525da0
user2047 <cameron.senna@gmail.com>
2018-03-11 21:20:39 -0400

    Fourth Commit

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 console.log("Server starting...");
+
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash")
 const notes = require("./notes.js");
 
-var user = os.userInfo();
-console.log("Hello " + user.username);
+console.log(_.isString("asdf"));
 
-fs.appendFile("greetings.txt", "Hello ${user.username}, you are ${notes.age}!", (err) => {
-  if (err) {
-    console.log("unknown error");
-  }});
+var filteredArray = _.uniq([1,1,2,2,3,3,4,4]);
+console.log(filteredArray)
+
+
+
+var result = notes.addNote();
+console.log(result);
+
+var res2 =notes.add(5,6);
+console.log(`Result: ${res2}`);
+
+// var user = os.userInfo();
+// console.log("Hello " + user.username);
+//
+// fs.appendFile("greetings.txt", `Hello ${user.username}, you are ${notes.age}!`, (err) => {
+//   if (err) {
+//     console.log("unknown error");
+//   }});
--- notes.js
+++ notes.js
@@ -1,2 +1,9 @@
 console.log("starting notes.js");
 module.exports.age = 21
+module.exports.addNote = () => {
+  console.log("added note");
+  return "new note";
+}
+module.exports.add = (a,b) => {
+  return a+b;
+}
