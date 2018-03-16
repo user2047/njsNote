fafd498e1c81b9cbc7433b596a2d258d95d6fbf1
user2047 <cameron.senna@gmail.com>
2018-03-16 02:14:01 -0400

    Fifth Commit

--- app.js
+++ app.js
@@ -4,19 +4,59 @@ const fs = require("fs");
 const os = require("os");
 const _ = require("lodash")
 const notes = require("./notes.js");
-
-console.log(_.isString("asdf"));
-
-var filteredArray = _.uniq([1,1,2,2,3,3,4,4]);
-console.log(filteredArray)
+const yargs = require("yargs");
 
 
+var command = process.argv[2]
+console.log("Command: ", command);
+//
+// console.log(process.argv);
+// console.log(yargs.argv)
 
-var result = notes.addNote();
-console.log(result);
+const argv = yargs.argv
 
-var res2 =notes.add(5,6);
-console.log(`Result: ${res2}`);
+
+
+if (command == "add") {
+  console.log("adding new note...");
+  notes.addNote(argv.title, argv.body);
+} else if (command == "list") {
+  console.log("listing notes...");
+  notes.getAll();
+}
+  else if (command =="read") {
+    console.log("reading note...");
+  }
+  else if (command == "remove") {
+    console.log("removing note...");
+  }
+  else {
+    console.log("Command not recognized");
+  }
+
+
+// console.log(process.argv)
+
+
+
+
+
+
+// console.log(_.isString("asdf"));
+
+
+
+
+// var filteredArray = _.uniq([1,1,2,2,3,3,4,4]);
+// console.log(filteredArray)
+//
+//
+//
+// var result = notes.addNote();
+// console.log(result);
+//
+// var res2 =notes.add(5,6);
+// console.log(`Result: ${res2}`);
 
 // var user = os.userInfo();
 // console.log("Hello " + user.username);
--- notes.js
+++ notes.js
@@ -1,9 +1,23 @@
-console.log("starting notes.js");
-module.exports.age = 21
-module.exports.addNote = () => {
-  console.log("added note");
-  return "new note";
+// console.log("starting notes.js");
+// module.exports.age = 21
+// module.exports.addNote = () => {
+//   console.log("added note");
+//   return "new note";
+// }
+// module.exports.add = (a,b) => {
+//   return a+b;
+// }
+console.log("starting notes.js...")
+function addNote(title,body) {
+  console.log("adding note...",title,body);
+
 }
-module.exports.add = (a,b) => {
-  return a+b;
+function getAll() {
+  console.log("listing all...");
 }
+
+
+module.exports = {
+  addNote
+  getAll
+};
